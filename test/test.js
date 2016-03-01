0bcfbec2993580b030d606849bddf86d79b5b215
Reto Gmür <reto@apache.org>
2016-03-01 17:19:24 +0000

    Issue #4: testing the existing (little known) support for datatypes

--- test/test.js
+++ test/test.js
@@ -35,6 +35,35 @@ describe('RDF2h', function () {
                 });
             });
         });
+        
+        it('Render dataype using pseudo property.', function () {
+            var dataTurtle = '@prefix dc: <http://dublincore.org/2012/06/14/dcelements#>. \n\
+                @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n\
+                <http://example.org/> dc:title "10"^^xsd:integer.';
+            var matchersTurtle = '@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n\
+                @prefix r2h: <http://rdf2h.github.io/2015/rdf2h#> .\n\
+                @prefix dc: <http://dublincore.org/2012/06/14/dcelements#>.\n\
+                [ a r2h:Matcher ;\n\
+                  r2h:triplePattern [\n\
+                    r2h:subject r2h:this;\n\
+                    r2h:predicate dc:title;\n\
+                  ];\n\
+                  r2h:template [ \n\
+                    r2h:context r2h:Default;\n\
+                    r2h:mustache "The type title: {{{dc:title/rdf:type}}}"\n\
+                  ]\n\
+                ].';
+            //mimeTypeUtil.parsers.parse('text/turtle', 
+            RDF2h.prefixMap['dc'] = "http://dublincore.org/2012/06/14/dcelements#";
+            return N3Parser.parse(matchersTurtle).then(function (matchers) {
+                return N3Parser.parse(dataTurtle).then(function (data) {
+                    //RDF2h.logger.setLevel(Logger.DEBUG);
+                    var renderingResult = new RDF2h(matchers).render(data, "http://example.org/");
+                    console.log("result: "+renderingResult);
+                    assert.equal("The type title: http://www.w3.org/2001/XMLSchema#integer", renderingResult);
+                });
+            });
+        });
 
         it('Once again but with a newline in data', function () {
             var dataTurtle = '@prefix dc: <http://dublincore.org/2012/06/14/dcelements#>. \n\
