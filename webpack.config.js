e394de7612682313ea0ad5ecfa6916b9a14a34eb
Reto Gmür <me@farewellutopia.com>
2018-01-03 23:02:14 +0100

    this version is debuggable in vscode, but no life updates

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+// This library allows us to combine paths easily
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+   entry: path.resolve(__dirname, 'src', 'index.js'),
+   output: {
+      path: path.resolve(__dirname, 'output', 'js'),
+      filename: 'bundle.js'
+   },
+   resolve: {
+      extensions: ['.js', '.jsx']
+   },
+   module: {
+      rules: [
+         {
+             test: /\.js/,
+             use: {
+                loader: 'babel-loader',
+                options: { presets: ['react', 'es2015'] }
+             }
+         }
+      ]
+   },
+   externals: {
+     'node-fetch': 'fetch',
+     'xmldom': 'window',
+     'rdflib': "$rdf"
+   },
+   devtool: 'source-map',
+   plugins: [
+    new HtmlWebpackPlugin({
+      filename: '../index.html',
+      title: 'Try RDF2h in your browser',
+      template: 'webpack-templates/index.ejs', // Load a custom template (ejs by default see the FAQ for details) 
+    })
+  ]
+};
