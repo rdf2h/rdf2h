cbd6e0e5e710f4abf881e91b26014f9317531d13
Reto Gmür <reto@factsmission.com>
2018-01-05 21:08:21 +0100

    first simplified typed based templates

--- test/test.js
+++ test/test.js
@@ -7,36 +7,57 @@ var rdf = require('rdflib');
 
 describe('RDF2h', function () {
   describe('#render()', function () {
-    it('Applying a simple template, using default prefixes.', function () {
+    it('Applying rdf:Resource template to untyped resource.', function () {
       var dataTurtle = '@prefix dc: <http://dublincore.org/2012/06/14/dcelements#>. \n\
                 <http://example.org/> dc:title "An example".';
       var matchersTurtle = '@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n\
+                @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n\
                 @prefix r2h: <http://rdf2h.github.io/2015/rdf2h#> .\n\
                 @prefix dc: <http://dublincore.org/2012/06/14/dcelements#>.\n\
-                [ a r2h:Matcher ;\n\
-                  r2h:triplePattern [\n\
-                    r2h:subject r2h:this;\n\
-                    r2h:predicate dc:title;\n\
-                  ];\n\
-                  r2h:template [ \n\
-                    r2h:context r2h:Default;\n\
-                    r2h:mustache "The title: {{dc:title}}"\n\
-                  ]\n\
+                [ a r2h:Template; \n\
+                  r2h:type rdfs:Resource;\n\
+                  r2h:context r2h:Default;\n\
+                  r2h:mustache "The title: {{dc:title}}"\n\
                 ].';
-      //mimeTypeUtil.parsers.parse('text/turtle', 
       RDF2h.prefixMap['dc'] = "http://dublincore.org/2012/06/14/dcelements#";
       var matchers = rdf.graph();
       rdf.parse(matchersTurtle, matchers, "http://example.org/matchers/", "text/turtle");
       var data = rdf.graph();
       rdf.parse(dataTurtle, data, "http://example.org/data", "text/turtle");
-
-      //RDF2h.logger.setLevel(Logger.DEBUG);
       var renderingResult = new RDF2h(matchers).render(data, "http://example.org/");
       console.log("result: " + renderingResult);
       assert.equal("The title: An example", renderingResult);
-
     });
-
+    it('Applying rdf:Resource template to untyped resource.', function () {
+      var dataTurtle = '@prefix dc: <http://dublincore.org/2012/06/14/dcelements#>. \n\
+                @prefix schema: <http://schema.org/>. \n\
+                <http://example.org/> dc:title "An example". \n\
+                <http://example.org/> a schema:Article .';
+      var matchersTurtle = '@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n\
+                @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n\
+                @prefix r2h: <http://rdf2h.github.io/2015/rdf2h#> .\n\
+                @prefix schema: <http://schema.org/>. \n\
+                @prefix dc: <http://dublincore.org/2012/06/14/dcelements#>.\n\
+                [ a r2h:Template; \n\
+                  r2h:type rdfs:Resource;\n\
+                  r2h:context r2h:Default;\n\
+                  r2h:mustache "This must not be chosen"\n\
+                ].\n\
+                [ a r2h:Template; \n\
+                  r2h:type schema:Article;\n\
+                  r2h:context r2h:Default;\n\
+                  r2h:mustache "The title: {{dc:title}}"\n\
+                ].';
+      RDF2h.prefixMap['dc'] = "http://dublincore.org/2012/06/14/dcelements#";
+      var matchers = rdf.graph();
+      rdf.parse(matchersTurtle, matchers, "http://example.org/matchers/", "text/turtle");
+      var data = rdf.graph();
+      rdf.parse(dataTurtle, data, "http://example.org/data", "text/turtle");
+      var renderingResult = new RDF2h(matchers).render(data, "http://example.org/");
+      console.log("result: " + renderingResult);
+      assert.equal("The title: An example", renderingResult);
+    });
+/*
     it('Applying a simple template with inverse property.', function () {
       var dataTurtle = '@prefix foaf: <http://xmlns.com/foaf/0.1/>. \n\
                 <http://example.org/a> foaf:knows <http://example.org/b>.';
@@ -296,7 +317,7 @@ describe('RDF2h', function () {
       console.log("result: "+renderingResult);
       assert.equal("The title: \nAn example", renderingResult);
     });
-    
+    */
     /*UNFINISHED: problem with rdflib turtle parser not supporting list syntax
     it('list rendering', function () {
       var dataTurtle = '@base <http://example.org/> .\n\
