602febcff67301159b2a1c9eb60a8ef9a7803247
Reto Gmür <reto@factsmission.com>
2019-10-02 14:10:11 +0200

    using new syntax here too

--- src/vocab.js
+++ src/vocab.js
@@ -1,19 +1,19 @@
-var $rdf = require("ext-rdflib");
+import rdf from "ext-rdflib";
 
 module.exports = {
     schema: function (suffix) {
-        return $rdf.sym("http://schema.org/" + suffix);
+        return rdf.sym("http://schema.org/" + suffix);
     },
     rdf: function (suffix) {
-        return $rdf.sym("http://www.w3.org/1999/02/22-rdf-syntax-ns#" + suffix);
+        return rdf.sym("http://www.w3.org/1999/02/22-rdf-syntax-ns#" + suffix);
     },
     rdfs: function (suffix) {
-        return $rdf.sym("http://www.w3.org/2000/01/rdf-schema#" + suffix);
+        return rdf.sym("http://www.w3.org/2000/01/rdf-schema#" + suffix);
     },
     foaf: function (suffix) {
-        return $rdf.sym("http://xmlns.com/foaf/0.1/" + suffix);
+        return rdf.sym("http://xmlns.com/foaf/0.1/" + suffix);
     },
     rdf2h: function(suffix) {
-        return $rdf.sym("http://rdf2h.github.io/2015/rdf2h#"+suffix);
+        return rdf.sym("http://rdf2h.github.io/2015/rdf2h#"+suffix);
     }
 }
